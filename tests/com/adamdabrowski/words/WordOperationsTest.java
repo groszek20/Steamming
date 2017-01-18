ba4166856b3b4d37219c3e3234646eac68c3dc64
adam.dabrowski <adamrobert.dabrowski@performgroup.com>
2017-01-18 13:49:29 +0100

    Test update

---
+++ tests/com/adamdabrowski/words/WordOperationsTest.java
@@ -0,0 +1,23 @@
+package com.adamdabrowski.words;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by adam on 18/01/2017.
+ */
+public class WordOperationsTest {
+    @Test
+    public void wordFormat() throws Exception {
+        WordOperations wordOperations = new WordOperations();
+        assertEquals("słowo", wordOperations.wordFormat(",@słowo,"));
+    }
+
+    @Test
+    public void setWordStem() throws Exception {
+        WordOperations wordOperations = new WordOperations();
+        assertEquals("wiek", wordOperations.setWordStem("wieku"));
+    }
+
+}

d559962f5d9a47f403387256ec65360df2f2a4a5
groszek20 <pea@op.pl>
2017-01-23 21:54:03 +0100

    Refactor

--- src/main/java/com/adamdabrowski/main/Main.java
+++ src/main/java/com/adamdabrowski/main/Main.java
@@ -1,35 +1,25 @@
 package com.adamdabrowski.main;
 
+import com.adamdabrowski.words.FileOperations;
 import com.adamdabrowski.words.WordOperations;
-import com.sun.javafx.scene.control.skin.VirtualFlow;
-import com.sun.xml.internal.fastinfoset.util.CharArray;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import morfologik.stemming.WordData;
-import morfologik.stemming.polish.PolishStemmer;
 
 public class Main {
 
     public static void main(String[] args) {
+        
         try {
+            FileOperations fileOperations = new FileOperations("source.txt");
             WordOperations wordOperation = new WordOperations();
-            FileReader fileReader = new FileReader("source.txt");
             PrintWriter wordWritter = new PrintWriter("output.txt");
-            BufferedReader buff = new BufferedReader(fileReader);
-            String line = buff.readLine();
+            String line = fileOperations.bufferedReadLine();
             StringTokenizer stringTokenizer = new StringTokenizer(line);
             String element = "";
             while (stringTokenizer.hasMoreElements()) {
@@ -37,7 +27,7 @@ public class Main {
                 element = wordOperation.wordFormat(element).toLowerCase();
                 wordWritter.print(wordOperation.setWordStem(element)+ " ");
             }
-            fileReader.close();
+            fileOperations.closeFileReader();
             wordWritter.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
